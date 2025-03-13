3a9b045566b3ece74b14472299e968aa18a38310
gtomasa <sarumasaki24@gmail.com>
2025-03-13 19:06:21 +0900

    お試し

--- test/test.java
+++ test/test.java
@@ -1,5 +1,12 @@
 public class Hello {
+  public static int math(String[] args){
+    int a = 10;
+    int b = 20;
+    return a + b;
+  }
   public static void main(String[] args) {
+    int result = math();
       System.out.println("Hello, " + args[0] + "!");
   }
 }
+
